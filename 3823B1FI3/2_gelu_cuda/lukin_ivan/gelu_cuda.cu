b47693154e61169086e9d9efa54df699b9afef03
Иван Лукин <dizbvalkyre@gmail.com>
2026-03-15 18:44:24 +0300

    delete alloc checks for perf (+0.015s)

--- 3823B1FI3/2_gelu_cuda/lukin_ivan/gelu_cuda.cu
+++ 3823B1FI3/2_gelu_cuda/lukin_ivan/gelu_cuda.cu
@@ -1,6 +1,12 @@
- #include "gelu_cuda.h"
+#include "gelu_cuda.h"
 
 #include <cuda_runtime.h>
+#include <iostream>
+#include <chrono>
+
+#pragma GCC optimize("O3")
+
+const int count = 134'217'728;
 
 __constant__ float PI_coeff = 0.797884F;
 __constant__ float coeff = 0.044715F;
@@ -28,31 +34,36 @@ std::vector<float> GeluCUDA(const std::vector<float>& input)
     float *output_gpu;
     const int bytes = size * sizeof(float);
 
-    if(cudaMalloc((void**)&input_gpu, bytes) != cudaSuccess) return input;
-    if(cudaMalloc((void**)&output_gpu, bytes) != cudaSuccess)
-    {
-        cudaFree((void*)input_gpu);
-        return input;
-    }
-    if(cudaMemcpy((void*)input_gpu, (void*)input.data(), bytes, cudaMemcpyHostToDevice) != cudaSuccess)
-    {
-        cudaFree((void*)input_gpu);
-        cudaFree((void*)output_gpu);
-        return input;
-    }
+    cudaMalloc((void**)&input_gpu, bytes);
+    cudaMalloc((void**)&output_gpu, bytes);
 
+    cudaMemcpy((void*)input_gpu, (void*)input.data(), bytes, cudaMemcpyHostToDevice);
     gelu_kernel <<<num_blocks, block_size>>> (input_gpu, output_gpu, size);
 
     std::vector<float> output(size);
-    if(cudaMemcpy((void*)output.data(), (void*)output_gpu, bytes, cudaMemcpyDeviceToHost) != cudaSuccess)
-    {
-        cudaFree((void*)input_gpu);
-        cudaFree((void*)output_gpu);
-        return input;
-    }
+    cudaMemcpy((void*)output.data(), (void*)output_gpu, bytes, cudaMemcpyDeviceToHost);
 
     cudaFree((void*)input_gpu);
     cudaFree((void*)output_gpu);
 
     return output;
+}
+
+int main()
+{
+    std::vector<float> input(count, 0.0F);
+    std::vector<float> output(count);
+    std::vector<double> time_list;
+    for (int i = 0; i < 4; ++i) {
+        auto start = std::chrono::high_resolution_clock::now();
+        output = GeluCUDA(input);
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> duration = end - start;
+        time_list.push_back(duration.count());
+    }
+    int i = 0;
+    std::cout << output[i += 10'000'000] << " ";
+    std::cout << std::endl;
+    double time = *std::min_element(time_list.begin(), time_list.end());
+    std::cout << "Time: " << time <<std::endl;
 }
