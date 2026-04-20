bbef3e59cbeece312ac1cb99112992a697a21ba9
fartekool <levonychev2006@gmail.com>
2026-04-20 20:22:24 +0300

    final version

--- 3823B1FI3/3_naive_gemm_omp/levonychev_ivan/naive_gemm_omp.cpp
+++ 3823B1FI3/3_naive_gemm_omp/levonychev_ivan/naive_gemm_omp.cpp
@@ -1,27 +1,24 @@
+#pragma GCC optimize("Ofast")
 #include "naive_gemm_omp.h"
 #include <vector>
-
+#include <omp.h>
 
 
 
 std::vector<float> NaiveGemmOMP(const std::vector<float>& a,
                                 const std::vector<float>& b,
                                 int n) {
-
-    std::vector<float> C(n * n);
-    
-    
+    std::vector<float> C(n * n, 0.0f);
+    // i -> k -> j (for simd)
+    #pragma omp parallel for
     for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-
-            float sum = 0.0f;
-
-            for (int k = 0; k < n; ++k) {
-                sum += a[i * n + k] * b[k * n + j];
+        for (int k = 0; k < n; ++k) {
+            float a_i_k = a[i * n + k];
+            #pragma omp simd
+            for (int j = 0; j < n; ++j) {
+                C[i * n + j] += a_i_k * b[k * n + j];
             }
-            C[i * n + j] = sum;
         }
     }
-
     return C;
 }
